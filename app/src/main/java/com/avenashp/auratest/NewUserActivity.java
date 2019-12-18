d5198c62b78b28a548c1f963a34b241d3205e0d9
AvenashP <48545411+AvenashP@users.noreply.github.com>
2019-12-18 14:49:24 +0530

    NewUserActivity completed

--- app/src/main/java/com/avenashp/auratest/NewUserActivity.java
+++ app/src/main/java/com/avenashp/auratest/NewUserActivity.java
@@ -1,31 +1,16 @@
 package com.avenashp.auratest;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseException;
-import com.google.firebase.FirebaseTooManyRequestsException;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.PhoneAuthCredential;
-import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.concurrent.TimeUnit;
 
 public class NewUserActivity extends AppCompatActivity {
 
